df2ba5f28e1a948b01fb9d23575a464b97d940a5
mash8 <43815223+mash8@users.noreply.github.com>
2024-11-12 20:44:42 +1100

    just testing if this might work in a pipeline?

--- basic.ts
+++ basic.ts
@@ -1,8 +1,23 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
-
 import type { Sodium } from "./basic_types.ts";
-import sodium from "./dist/browsers/sodium.js";
+import sodium from "./dist/browsers/sodium2.js";
 
-export default sodium as Sodium;
+const sodiumLib: Record<string, any> = {};
+
+const entries = Object.entries(((sodium as any).libsodium)).concat(Object.entries(((sodium as any).sodium)));
+
+for (const entry of entries) {
+    const prop = entry[0];
+    const val = entry[1];
+
+    const newPropName = prop.startsWith("_") ? prop.slice(1) : prop;
+
+    if (sodiumLib[newPropName] === undefined) {
+        sodiumLib[newPropName] = val;
+    }
+}
+
+
+export default sodiumLib as Sodium;
 
 export * from "./basic_types.ts";
